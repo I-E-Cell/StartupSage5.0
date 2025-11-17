1e3f896693e4bc70cbb3fc8b40b1f5115cad20cc
pinak <viveksharma_240087@aitpune.edu.in>
2025-11-17 13:48:30 +0530

    added minor details

--- src/section/Hero.jsx
+++ src/section/Hero.jsx
@@ -1,28 +1,45 @@
-import React from 'react'
-import Navbar from './Navbar'
-import Loginbtn from '../components/buttons/Loginbtn'
-import Signupbtn from '../components/buttons/Signupbtn'
+import React from "react";
+import Navbar from "./Navbar";
+import Loginbtn from "../components/buttons/Loginbtn";
+import Signupbtn from "../components/buttons/Signupbtn";
+import treasure from "/treasure.svg";
+import moon from "/moon.svg";
 
 function Hero() {
   return (
     <>
-        <div className='flex w-full h-full flex-col justify-center items-start font-semibold relative'>
-            <p className='absolute top-[5%] left-[45%]'>[NextNiche]</p>
-            <p className='absolute top-[25%] left-[20%]'>[SharkTank]</p>
-            <p className='absolute top-[60%] left-[45%]'>[StartupExpo]</p>
-            <p className='absolute top-[75%] left-[33%]'>[StandUp]</p>
-            <p className='text-8xl'>Startup Saga 5.0</p>
-            <p className='text-8xl'>Coming Soon</p>
+      <div className="flex w-full h-full flex-col gap-y-2 justify-center items-start font-semibold relative">
+        <div className="mb-4 self-center">
+            <p>[SharkTank AIT]</p>
         </div>
-        <div className='flex flex-row justify-between w-full mb-16 border-b py-4'>
-            <div><p>/ Play the Game, Earn the Profits!</p></div>
-            <div className='flex flex-row gap-3'>
-                <Loginbtn label='Login'/>
-                <Signupbtn label='Sign UP'/>
+        <div className="ml-[20%]">
+            <p>[NextNiche]</p>
+        </div>
+        <div>
+            <div className="flex flex-row">
+                <p className="text-8xl">Startup Saga 5.0</p>
+                <img src={treasure} className="h-32" alt="" />
+            </div>
+            <div className="flex flex-row w-full items-center gap-x-8">
+                <p className="text-8xl text-nowrap">Coming</p>
+                <img src={moon} className="h-32" alt="" />
+                <p className="text-8xl text-nowrap">Coming</p>
+                <p className="pl-[5%] pt-6">[Expo]</p>
             </div>
         </div>
+        <div className="ml-[35%] mt-6">[StandUp]</div>
+      </div>
+      <div className="flex flex-row justify-between w-full mb-16 border-b py-4">
+        <div>
+          <p>/ Play the Game, Earn the Profits!</p>
+        </div>
+        <div className="flex flex-row gap-3">
+          <Loginbtn label="Login" />
+          <Signupbtn label="Sign UP" />
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
